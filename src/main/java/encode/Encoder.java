7a875dd92520a03749d3db818f6fb1b8b20d2816
corvinusNWH9YB <mate.fenyvesvolgyi@stud.uni-corvinus.hu>
2021-02-16 09:51:40 +0100

    gitignore+typo

--- src/main/java/encode/Encoder.java
+++ src/main/java/encode/Encoder.java
@@ -6,32 +6,32 @@ public class Encoder {
     private final String inputFilePath;
     private final String outputFilePath;
 
-    public Encoder(String inputFilePath, String outputFilePath){
+    public Encoder(String inputFilePath, String outputFilePath) {
         this.inputFilePath = inputFilePath;
         this.outputFilePath = outputFilePath;
     }
 
     /**
-     * Karakterek eltolása eggyel és ennek rögzítése az outputfile-ba.
+     * Karakterek eltolása eggyel és ennek rögzítése az outputfile-ban.
      */
-    public void encode(){
+    public void encode() {
         FileReader fr = null;
         FileWriter fw = null;
         try {
             fr = new FileReader(inputFilePath);
-            BufferedReader br=new BufferedReader(fr);
+            BufferedReader br = new BufferedReader(fr);
             fw = new FileWriter(outputFilePath);
-            BufferedWriter bw=new BufferedWriter(fw);
+            BufferedWriter bw = new BufferedWriter(fw);
             int i;
             //Végig megyünk a charactereken és eltoljuk eggyel
-            while ((i=br.read()) != -1) {
-                bw.append((char)(i+1));
+            while ((i = br.read()) != -1) {
+                bw.append((char) (i + 1));
             }
             bw.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found:" + e.getMessage());
-        } catch (IOException e){
-            System.out.println("IO Exception: "+ e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO Exception: " + e.getMessage());
         }
     }
 }
